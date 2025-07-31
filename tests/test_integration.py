f444f6378deb7982ad32ac6bad303d4f0af221dd
Travis Denny <3370329+tdenny@users.noreply.github.com>
2025-07-31 09:37:43 -0500

    add integration test to check prometheus scrapes our metric

---
+++ tests/test_integration.py
@@ -0,0 +1,43 @@
+import json
+import requests
+from deepdiff import DeepDiff
+from time import sleep
+
+def test_prometheus_scrapes_metrics():
+    with open('tests/sample_payload.json', 'r') as f:
+        test_payload = json.load(f)
+    app_response = requests.post('http://localhost:8080/webhook',
+                                 json=test_payload)
+
+    app_response.raise_for_status()
+
+    prom_query = {"query": 'githubactions_workflow_run_total{workflow_id="1"}'}
+    expected_query_result = {
+                              "status": "success",
+                              "data": {
+                                "resultType": "vector",
+                                "result": [
+                                  {
+                                    "metric": {
+                                      "__name__": "githubactions_workflow_run_total",
+                                      "instance": "gha_prometheus:80",
+                                      "job": "gha_prometheus",
+                                      "workflow_id": "1"
+                                    },
+                                    "value": [
+                                      1753968692.312,
+                                      "1"
+                                    ]
+                                  }
+                                ]
+                              }
+                            }
+
+    sleep(5)
+
+    prometheus_response = requests.get('http://localhost:9090/api/v1/query',
+                                       params=prom_query)
+
+    assert {} == DeepDiff(prometheus_response.json(),
+                          expected_query_result,
+                          exclude_paths=["root['data']['result'][0]['value'][0]"])
