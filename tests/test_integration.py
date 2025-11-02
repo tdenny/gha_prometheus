9c5a7dbb590c649149d0125971ae8c17ce8e1ecc
Travis Denny <3370329+tdenny@users.noreply.github.com>
2025-11-02 10:21:03 -0600

    add integration test helper class for inspecting prometheus responses

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -1,7 +1,21 @@
 import json
 import requests
-from deepdiff import DeepDiff
 from time import sleep
+
+class PrometheusResponse:
+    """
+    Helper function to represent a response from a prometheus query.
+
+    In the context of these tests, this class expects a single result object
+    from prometheus queries. It also ignores the timestamp returned in the
+    results.
+    """
+
+    def __init__(self, raw_response):
+        self.status = raw_response['status']
+        self.result_type = raw_response['data']['resultType']
+        self.metric_name = raw_response['data']['result'][0]['metric']['__name__']
+        self.metric_value = raw_response['data']['result'][0]['value'][1]
 
 def send_test_payload(payload_file, payload_event):
     webhook_endpoint = 'http://localhost:8080/webhook'
@@ -20,7 +34,7 @@ def query_prometheus(query):
 
     response.raise_for_status()
 
-    return response.json()
+    return PrometheusResponse(response.json())
 
 def test_prometheus_scrapes_metrics():
     send_test_payload('tests/payloads/valid_workflow_run.json', 'workflow_run')
@@ -30,173 +44,38 @@ def test_prometheus_scrapes_metrics():
 
     sleep(5)
 
-    expected_query_result = {
-                              "status": "success",
-                              "data": {
-                                "resultType": "vector",
-                                "result": [
-                                  {
-                                    "metric": {
-                                      "__name__": "githubactions_workflow_run_total",
-                                      "instance": "gha_prometheus:80",
-                                      "job": "gha_prometheus",
-                                      "workflow_id": "1"
-                                    },
-                                    "value": [
-                                      1753968692.312,
-                                      "2"
-                                    ]
-                                  }
-                                ]
-                              }
-                            }
-
     prom_query = {"query": 'githubactions_workflow_run_total{workflow_id="1"}'}
     prometheus_response = query_prometheus(prom_query)
 
-    assert {} == DeepDiff(prometheus_response,
-                          expected_query_result,
-                          exclude_paths=["root['data']['result'][0]['value'][0]"])
-
-    expected_query_result = {
-                              "status": "success",
-                              "data": {
-                                "resultType": "vector",
-                                "result": [
-                                  {
-                                    "metric": {
-                                      "__name__": "githubactions_workflow_success_total",
-                                      "instance": "gha_prometheus:80",
-                                      "job": "gha_prometheus",
-                                      "workflow_id": "1"
-                                    },
-                                    "value": [
-                                      1753968692.312,
-                                      "1"
-                                    ]
-                                  }
-                                ]
-                              }
-                            }
+    assert prometheus_response.metric_name == "githubactions_workflow_run_total"
+    assert prometheus_response.metric_value == "2"
 
     prom_query = {"query": 'githubactions_workflow_run_success_total{workflow_id="1"}'}
     prometheus_response = query_prometheus(prom_query)
 
-    assert {} == DeepDiff(prometheus_response,
-                          expected_query_result,
-                          exclude_paths=["root['data']['result'][0]['value'][0]"])
-
-    expected_query_result = {
-                              "status": "success",
-                              "data": {
-                                "resultType": "vector",
-                                "result": [
-                                  {
-                                    "metric": {
-                                      "__name__": "githubactions_workflow_failure_total",
-                                      "instance": "gha_prometheus:80",
-                                      "job": "gha_prometheus",
-                                      "workflow_id": "1"
-                                    },
-                                    "value": [
-                                      1753968692.312,
-                                      "1"
-                                    ]
-                                  }
-                                ]
-                              }
-                            }
+    assert prometheus_response.metric_name == "githubactions_workflow_run_success_total"
+    assert prometheus_response.metric_value == "1"
 
     prom_query = {"query": 'githubactions_workflow_run_failure_total{workflow_id="1"}'}
     prometheus_response = query_prometheus(prom_query)
 
-    assert {} == DeepDiff(prometheus_response,
-                          expected_query_result,
-                          exclude_paths=["root['data']['result'][0]['value'][0]"])
-
-    expected_query_result = {
-                              "status": "success",
-                              "data": {
-                                "resultType": "vector",
-                                "result": [
-                                  {
-                                    "metric": {
-                                      "__name__": "githubactions_workflow_job_total",
-                                      "instance": "gha_prometheus:80",
-                                      "job": "gha_prometheus",
-                                      "workflow_job_id": "48831014846",
-                                      "workflow_run_id": "30041"
-                                    },
-                                    "value": [
-                                      1753968692.312,
-                                      "2"
-                                    ]
-                                  }
-                                ]
-                              }
-                            }
+    assert prometheus_response.metric_name == "githubactions_workflow_run_failure_total"
+    assert prometheus_response.metric_value == "1"
 
     prom_query = {"query": 'githubactions_workflow_job_total{workflow_run_id="30041"}'}
     prometheus_response = query_prometheus(prom_query)
 
-    assert {} == DeepDiff(prometheus_response,
-                          expected_query_result,
-                          exclude_paths=["root['data']['result'][0]['value'][0]"])
-
-    expected_query_result = {
-                              "status": "success",
-                              "data": {
-                                "resultType": "vector",
-                                "result": [
-                                  {
-                                    "metric": {
-                                      "__name__": "githubactions_workflow_job_success_total",
-                                      "instance": "gha_prometheus:80",
-                                      "job": "gha_prometheus",
-                                      "workflow_job_id": "48831014846",
-                                      "workflow_run_id": "30041"
-                                    },
-                                    "value": [
-                                      1753968692.312,
-                                      "1"
-                                    ]
-                                  }
-                                ]
-                              }
-                            }
+    assert prometheus_response.metric_name == "githubactions_workflow_job_total"
+    assert prometheus_response.metric_value == "2"
 
     prom_query = {"query": 'githubactions_workflow_job_success_total{workflow_run_id="30041"}'}
     prometheus_response = query_prometheus(prom_query)
 
-    assert {} == DeepDiff(prometheus_response,
-                          expected_query_result,
-                          exclude_paths=["root['data']['result'][0]['value'][0]"])
-
-    expected_query_result = {
-                              "status": "success",
-                              "data": {
-                                "resultType": "vector",
-                                "result": [
-                                  {
-                                    "metric": {
-                                      "__name__": "githubactions_workflow_job_failure_total",
-                                      "instance": "gha_prometheus:80",
-                                      "job": "gha_prometheus",
-                                      "workflow_job_id": "48831014846",
-                                      "workflow_run_id": "30041"
-                                    },
-                                    "value": [
-                                      1753968692.312,
-                                      "1"
-                                    ]
-                                  }
-                                ]
-                              }
-                            }
+    assert prometheus_response.metric_name == "githubactions_workflow_job_success_total"
+    assert prometheus_response.metric_value == "1"
 
     prom_query = {"query": 'githubactions_workflow_job_failure_total{workflow_run_id="30041"}'}
     prometheus_response = query_prometheus(prom_query)
 
-    assert {} == DeepDiff(prometheus_response,
-                          expected_query_result,
-                          exclude_paths=["root['data']['result'][0]['value'][0]"])
+    assert prometheus_response.metric_name == "githubactions_workflow_job_failure_total"
+    assert prometheus_response.metric_value == "1"
